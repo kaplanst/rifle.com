48d038e87bd66099d910e049fc1a795b890f6869
Tim Yoshi <tim.yoshi@gmail.com>
2021-12-13 00:46:43 -0500

    Added 5 (!!!) new test based on Selenium native POM in CartPageTest using updated CartPage model.

--- src/test/java/model/AccountPage.java
+++ src/test/java/model/AccountPage.java
@@ -3,10 +3,10 @@ package model;
 import org.openqa.selenium.WebDriver;
 
 public class AccountPage {
-    public AccountPage(WebDriver driver) {
-        this.driver = driver;
-    }
 
     WebDriver driver;
 
+    public AccountPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
