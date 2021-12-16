47f1ed559ddf90aa80a6a937f89022288f9dc0b6
Tim Yoshi <tim.yoshi@gmail.com>
2021-12-15 22:50:51 -0500

    Refactored code to POM Chain notation and added login routine method.

--- src/test/java/Utils/UtilsMethod.java
+++ src/test/java/Utils/UtilsMethod.java
@@ -1,4 +1,30 @@
 package Utils;
 
+import model.LoginPage;
+import model.MainPage;
+import org.openqa.selenium.WebDriver;
+
 public class UtilsMethod {
+    MainPage mainPage;
+    LoginPage loginPage;
+    WebDriver driver;
+
+    public UtilsMethod(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String login, String pass) {
+        mainPage = new MainPage(driver);
+        loginPage = mainPage.clickLoginButton();
+
+        loginPage = new LoginPage(driver);
+        loginPage.fillUsername(login);
+        loginPage.fillPassword(pass);
+        loginPage.clickSigninButton();
+        //return new MainPage(driver);
+    }
+
+//    public void clearCart(){
+//
+//    }
 }
