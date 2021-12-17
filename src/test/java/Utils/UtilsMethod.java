5864aefd96ed33b193bdf5e661204267481323bc
Stanislav <kaplanst@mail.ru>
2021-12-17 12:36:44 -0500

    LoginPageTest Class created

--- src/test/java/Utils/UtilsMethod.java
+++ src/test/java/Utils/UtilsMethod.java
@@ -1,7 +1,9 @@
 package Utils;
 
+import model.CartPage;
 import model.LoginPage;
 import model.MainPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class UtilsMethod {
@@ -19,6 +21,18 @@ public class UtilsMethod {
                  .fillUsername(login)
                  .fillPassword(pass)
                  .clickSigninButton();
+    }
+
+    public CartPage clearCart() {
+        try {
+            driver.findElement(By.xpath("//*[@id='ctl00_ctl00_NestedMaster_PageContent_ClearBasketButton']")).click();
+            driver.switchTo().alert().accept();
+            System.out.println("Cart contained some items");
+
+        } catch (Exception e){
+            System.out.println("Cart was empty");
+        }
+        return new CartPage(driver);
     }
 
 }
