14d896a1a7b9cfeacad908d8224a4a17519fd486
tim-yoshi <42818031+tim-yoshi@users.noreply.github.com>
2021-12-22 22:01:06 -0500

    File output + local.properties + Victoria utils (#20)

--- src/test/java/Utils/UtilsMethod.java
+++ src/test/java/Utils/UtilsMethod.java
@@ -4,9 +4,11 @@ import model.CartPage;
 import model.LoginPage;
 import model.MainPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class UtilsMethod {
+public class UtilsMethod extends BaseTest{
     MainPage mainPage;
     LoginPage loginPage;
     WebDriver driver;
@@ -21,6 +23,27 @@ public class UtilsMethod {
                  .fillUsername(login)
                  .fillPassword(pass)
                  .clickSigninButton();
+    }
+
+    public void loginDefault() {
+        mainPage = new MainPage(driver);
+        loginPage = mainPage.clickLoginButton()
+                .fillUsername(getUserName());
+        System.out.println(getUserName());
+        loginPage.fillPassword(getUserPassword());
+        System.out.println(getUserPassword());
+        loginPage.clickSigninButton();
+    }
+
+    public void scroll(WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void scrollClick(WebDriver driver, By by) {
+        WebElement element = driver.findElement(by);
+        scroll(driver, element);
+        element.click();
     }
 
     public CartPage clearCart() {
