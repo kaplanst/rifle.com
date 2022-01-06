0b54ad522e8f58f98c92d11e8616366e65562acf
Stanislav <kaplanst@mail.ru>
2022-01-06 15:32:30 -0500

    Login Default method added

--- src/test/java/Roman.java
+++ src/test/java/Roman.java
@@ -11,31 +11,14 @@ public class Roman extends BaseTest {
     @Test
     void shoppingCartAddRemoveTest() throws InterruptedException { //TEST CASE TC-FT-008
         driver.get("https://rifle.com/4-Wheel-Off-Road/Suzuki-Samurai/Suzuki-Samurai-Under-Seat-Storage-Tray.aspx");
-        driver.manage().window().maximize();
-
         driver.findElement(By.xpath("//*[@class='btn btn-default']")).click();
-
         driver.findElement(By.xpath("//*[@id='ctl00_ctl00_NestedMaster_PageFooter_StoreFooterRifle_F_BasketLink']")).click();
-
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='fieldHeader']")).getText(), "Subtotal:");
 
         driver.findElement(By.xpath("//*[@id='ctl00_ctl00_NestedMaster_PageContent_ClearBasketButton']")).click();
-
         driver.switchTo().alert().accept();
-
         Thread.sleep(2000);
-
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='message']")).getText(), "Your cart is empty.");
 
-        driver.quit();
-
     }
-
-    void shoppingCartMultipleQuantityEditTest(){
-
-        driver.get("https://rifle.com/4-Wheel-Off-Road/Suzuki-Samurai/Suzuki-Samurai-Under-Seat-Storage-Tray.aspx");
-
-
-    }
-
 }
